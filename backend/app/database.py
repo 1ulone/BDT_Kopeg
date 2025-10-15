cfade8316a0056c03f0649c4b53de8f572f5db97
luthfiyyah <luthfiyyahsutoyo@gmail.com>
2025-10-15 14:40:03 +0700

    push

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,11 +1,14 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from motor.motor_asyncio import AsyncIOMotorClient
 
 load_dotenv()
 
-client = MongoClient('mongodb://admin:password@localhost:27017/')
+async_client = AsyncIOMotorClient('mongodb://localhost:27017/')
+client = MongoClient('mongodb://localhost:27017/')
 
 db = client["db_kopeg"]
+db_async = async_client["db_kopeg"]
 pembelian_collection = db["pembelian"]
 penjualan_collection = db["penjualan"]
 pengembalian_collection = db["pengembalian"]
